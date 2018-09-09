29862752da60a2f483afe855bcc896ec04c2c160
Roberto Samia <bojosteph@gmail.com>
2018-09-09 13:18:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -2,11 +2,7 @@ def welcome
   puts "Welcome to the Black jack Table"
 end
 
-def deal_card
-  card = rand(1..11)
-  card_total = card += card
-  puts "Your cards add up to #{card_total}"
-end
+
 
 
 
